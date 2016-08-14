001e558e94f9b06b776f4c51f397686f12786f84
John Gallagher <john@synapticmishap.co.uk>
2016-08-14 12:25:11 +0100

    Added rep example and pagination.

---
+++ app/models/components/ac_search_results_pagination.rb
@@ -0,0 +1,96 @@
+class Components::AcSearchResultsPagination
+  attr_reader :total_number_of_pages, :current_page
+  PATH = "/vehicles"
+
+  def initialize(state:, total_number_of_pages:, current_page:)
+    @state = state
+    @total_number_of_pages = total_number_of_pages
+    @current_page = current_page.to_i
+  end
+
+  def first_page? 
+    @current_page == 1
+  end
+
+  def last_page? 
+    @current_page == @total_number_of_pages
+  end
+
+  def first_page_link
+    if first_page?
+    %Q{
+      <span class="ac-pagination__button ac-pagination__button--first ac-pagination--disabled">First
+      </span>
+    }.html_safe
+    else
+    %Q{
+      <a data-page-number="1" href="#{page_path(1)}" class="ac-pagination__button ac-pagination__button--first">First
+      </a>
+    }.html_safe
+    end
+  end
+
+  def previous_page_link
+    if first_page?
+    %Q{
+      <span class="ac-pagination__button ac-pagination__button--previous ac-pagination--disabled">Previous
+      </span>
+    }.html_safe
+    else
+    %Q{
+      <a data-page-number="#{@current_page - 1}" href="#{page_path(@current_page - 1)}" class="ac-pagination__button ac-pagination__button--previous">Previous
+      </a>
+    }.html_safe
+    end
+  end
+
+  def page_link page_number
+    if page_number == @current_page
+    %Q{
+      <span class="ac-pagination__link ac-pagination__link--current ac-pagination--disabled">#{page_number}
+      </span>
+    }.html_safe
+    else
+      %Q{
+      <a data-page-number="#{page_number}" href="#{page_path(page_number)}" class="ac-pagination__link">#{page_number}
+      </a>
+      }.html_safe
+    end
+  end
+
+  def next_page_link
+    if last_page? 
+    %Q{
+      <span class="ac-pagination__button ac-pagination__button--next ac-pagination--disabled">Next
+      </span>
+    }.html_safe
+    else
+    %Q{
+      <a data-page-number="#{@current_page + 1}" href="#{page_path(@current_page + 1)}" class="ac-pagination__button ac-pagination__button--next">Next
+      </a>
+    }.html_safe
+    end
+  end
+
+
+  def last_page_link
+    if last_page? 
+    %Q{
+      <span class="ac-pagination__button ac-pagination__button--last ac-pagination--disabled">Last
+      </span>
+    }.html_safe
+    else
+    %Q{
+      <a data-page-number="#{@total_number_of_pages}" href="#{page_path(@total_number_of_pages)}" class="ac-pagination__button ac-pagination__button--last">Last
+      </a>
+    }.html_safe
+    end
+  end
+
+  private
+
+  def page_path page_number
+    "#{PATH}?make=#{@state['make']}&model=#{@state['model']}&search_type=#{@state['search_type']}&page=#{page_number}"
+  end
+
+end
--- app/models/components/ac_vehicle_search.rb
+++ app/models/components/ac_vehicle_search.rb
@@ -1,7 +1,7 @@
 class Components::AcVehicleSearch
-  attr_reader :vehicles, :makes, :models, :count, :search_types, :title
-  def initialize(make:, model:, search_type:)
-    url = "http://www.arnoldclark.com/used-cars/search.json?search_type=#{search_type}&make=#{make}&model=#{model}"
+  attr_reader :vehicles, :makes, :models, :count, :search_types, :title, :total_number_of_pages, :current_page
+  def initialize(state:)
+    url = "http://www.arnoldclark.com/used-cars/search.json?search_type=#{state[:search_type]}&make=#{state[:make]}&model=#{state[:model]}&page=#{state[:page]}"
     response = JSON.parse(open(url).read)
     @vehicles = response["searchResults"].map { |result| OpenStruct.new(result) }
     @makes = response["searchCriteria"]["availableOptions"]["makes"]
@@ -9,5 +9,8 @@ class Components::AcVehicleSearch
     @count = response["count"]
     @search_types = response["searchCriteria"]["availableOptions"]["searchTypes"]
     @title = response["title"]
+    @state = state
+    @total_number_of_pages = response["pagination"]["totalNumberOfPages"]
+    @current_page = response["pagination"]["currentPage"]
   end
 end
