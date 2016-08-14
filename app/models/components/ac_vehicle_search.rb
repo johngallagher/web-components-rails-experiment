18498671c6b6346df7413373f5766cba487bd148
John Gallagher <john@synapticmishap.co.uk>
2016-08-14 11:18:28 +0100

    Move to vehicles index.

--- app/models/components/ac_vehicle_search.rb
+++ app/models/components/ac_vehicle_search.rb
@@ -1,13 +1,13 @@
 class Components::AcVehicleSearch
-  attr_reader :vehicles, :makes, :models
-  def initialize(make:, model:, params:)
-    @make = make
-    @model = model
-    @params = params
-    url = "http://www.arnoldclark.com/used-cars/search.json?make=#{@make}&model=#{@model}"
-    search_response = JSON.parse(open(url).read)
-    @vehicles = search_response["searchResults"].map { |result| OpenStruct.new(result) }
-    @makes = search_response["searchCriteria"]["availableOptions"]["makes"]
-    @models = search_response["searchCriteria"]["availableOptions"]["models"]
+  attr_reader :vehicles, :makes, :models, :count, :search_types, :title
+  def initialize(make:, model:, search_type:)
+    url = "http://www.arnoldclark.com/used-cars/search.json?search_type=#{search_type}&make=#{make}&model=#{model}"
+    response = JSON.parse(open(url).read)
+    @vehicles = response["searchResults"].map { |result| OpenStruct.new(result) }
+    @makes = response["searchCriteria"]["availableOptions"]["makes"]
+    @models = response["searchCriteria"]["availableOptions"]["models"]
+    @count = response["count"]
+    @search_types = response["searchCriteria"]["availableOptions"]["searchTypes"]
+    @title = response["title"]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "home#show"
-  resource :home
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "vehicles#index"
+
+  resources :vehicles
 end
