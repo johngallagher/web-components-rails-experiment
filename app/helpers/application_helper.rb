9eb38f0b20bdd74705275ea2c8615651c6c87234
John Gallagher <john@synapticmishap.co.uk>
2016-08-16 00:33:30 +0100

    Load stylesheets from assets using a helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def stylesheet_for_component(name)
+    stylesheet = (Rails.application.assets || ::Sprockets::Railtie.build_environment(Rails.application)).find_asset("#{name}/#{name}").to_s
+    "<style>#{stylesheet}</style>".html_safe
+  end
 end
