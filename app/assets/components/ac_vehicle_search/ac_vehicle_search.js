95278c1a01f396980ebe5ec5e36dccfec163447e
John Gallagher <john@synapticmishap.co.uk>
2016-08-16 12:58:31 +0100

    Register component more concisely.

--- app/assets/components/ac_vehicle_search/ac_vehicle_search.js
+++ app/assets/components/ac_vehicle_search/ac_vehicle_search.js
@@ -1,6 +1,4 @@
-var AcVehicleSearch = {
+Nostalgia.registerComponent({
   name: "ac_vehicle_search",
   reloadOn: "ac_vehicle_search_filter.did_change"
-};
-
-Nostalgia.registerComponent(AcVehicleSearch);
+});
