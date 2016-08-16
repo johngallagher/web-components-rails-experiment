d43a2946eab429dc281564e6edf953f59adb7f16
John Gallagher <john@synapticmishap.co.uk>
2016-08-16 22:15:33 +0100

    Got rid of old controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,5 @@ Rails.application.routes.draw do
   root "home#show"
   resource :home
   resources :vehicles
-  resource :component_updates
-  namespace :api do
-    namespace :components do
-      resource :ac_vehicle_search, only: :show
-      resource :ac_vehicle_search_loading, only: :show
-    end
-  end
+  resource :component_updates, only: :show
 end
