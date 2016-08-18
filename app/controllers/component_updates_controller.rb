cd6bf24b5fc6eeceb80874f897a604a812546ee7
John Gallagher <john@synapticmishap.co.uk>
2016-08-19 00:00:42 +0100

    Got rid of cruft.

--- app/controllers/component_updates_controller.rb
+++ app/controllers/component_updates_controller.rb
@@ -3,7 +3,6 @@ class ComponentUpdatesController < ApplicationController
     @ac_vehicle_search = Components::AcVehicleSearch.new(params: params)
     render json: {
       component: "ac_vehicle_search_filter",
-      #replacements: components_to_reload.map { |component| { replace: component, with_content: render_to_string(partial: "components/#{component}", layout: false) } }
       replacements: replacements
     }
   end
