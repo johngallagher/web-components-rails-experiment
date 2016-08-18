c92c3c290b4d13fc9fd6a79f7de3252e59dee3b4
John Gallagher <john@synapticmishap.co.uk>
2016-08-18 20:54:38 +0100

    Refactored controller.

--- app/controllers/component_updates_controller.rb
+++ app/controllers/component_updates_controller.rb
@@ -1,10 +1,22 @@
 class ComponentUpdatesController < ApplicationController
   def show
     @ac_vehicle_search = Components::AcVehicleSearch.new(params: params)
-    components_to_reload = JSON.parse(params['onchange_reload'])
     render json: {
       component: "ac_vehicle_search_filter",
       replacements: components_to_reload.map { |component| { replace: component, with_content: render_to_string(partial: "components/#{component}", layout: false) } }
     }
+  end
+
+  def components_to_reload
+    JSON.parse(params['onchange_reload'])
+  end
+
+  def replacements
+    components_to_reload.map do |component|
+      {
+        replace: component,
+        with_content: render_to_string(partial: "components/#{component}", layout: false)
+      }
+    end
   end
 end
